350a26a17d0fd5887949aecd5f2f6a72c2a687b8
Humberto Fernández <humbertof44@gmail.com>
2025-10-21 00:19:13 -0400

    Add controllers and services to AppModule with HTTP module configuration

--- backend/business-service/src/app.module.ts
+++ backend/business-service/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+
+import { ClientController } from './controllers/client.controller';
+import { WalletController } from './controllers/wallet.controller';
+import { PaymentController } from './controllers/payment.controller';
+
+import { DatabaseHttpService } from './services/database-http.service';
+import { DatabaseClientService } from './services/database-client.service';
+import { PaymentService } from './services/payment.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HttpModule.register({
+      baseURL: 'http://localhost:3001',
+      timeout: 5000,
+    }),
+  ],
+  controllers: [
+    AppController,
+    ClientController,
+    WalletController,
+    PaymentController,
+  ],
+  providers: [
+    AppService,
+    DatabaseHttpService,
+    DatabaseClientService,
+    PaymentService,
+  ],
 })
 export class AppModule {}
